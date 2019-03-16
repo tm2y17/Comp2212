551ca713d9c0bad0986c6ff25290eba28dd23ad9
Mo,tianyu <mty1581817811@gmail.com>
2019-03-16 21:10:10 +0000

    update

--- Main.hs
+++ Main.hs
@@ -1,19 +1,31 @@
+import SplTokens
+import SplGrammar
+import System.Environment
+import Control.Exception
+import System.IO
 import Data.List
 
 main :: IO ()
-main = do 
-    content <-  getContents
-    putStrLn ("content: "++ "\n"++content)
-    putStrLn ("    ")
+main = catch main' noParse
 
-    putStrLn ("matrix: ")
-    let int_List = transfer (lines content)
-    print int_List
-    --int_List :: [[Int]] 
-    putStrLn ("    ")
+main' = do (fileName : _ ) <- getArgs 
+           sourceText <- readFile fileName
+           putStrLn ("Parsing : " ++ sourceText)
+           let parsedProg = parseCalc (alexScanTokens sourceText)
+           putStrLn ("Parsed as " ++ (show parsedProg) ++ "\n")
 
-    putStrLn ("standard output: ")
-    sequence_ $ (matrixToStringList int_List) >>= (\x -> [putStrLn x])
+           content <-  getContents
+           let int_List = transfer (lines content)
+           print int_List
+
+           --putStrLn ("    ")
+           --putStrLn ("standard output: ")
+           --sequence_ $ (matrixToStringList int_List) >>= (\x -> [putStrLn x])
+
+noParse :: ErrorCall -> IO ()
+noParse e = do let err =  show e
+               hPutStr stderr err
+               return ()
 
 transfer :: [String] -> [[Int]]
 transfer (x:xs) = [(map read $ words x :: [Int])] ++ transfer xs
