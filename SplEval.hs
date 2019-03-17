bfd7159fef5b9673755e8a1c84741992238f9658
Mo,tianyu <mty1581817811@gmail.com>
2019-03-17 03:42:17 +0000

    update

--- SplEval.hs
+++ SplEval.hs
@@ -1,100 +1,123 @@
 module SplEval where
-import SplGrammar
+    import SplGrammar
     
-data SplType = TyInt | TyBool | TyIntList | TyIntMatrix 
-    deriving (Show,Eq)
-
-type Environment = [(String,String,SplType)]
-type State = (Sentence,[Sentence],Environment)
-
-formatSentence :: Sentence -> [Sentence]
-formatSentence (SplConnecting x1 x2) = formatSentence x1 ++ formatSentence x2
-formatSentence x = [x]
-
-parseToState :: Sentence -> State
-parseToState s = (head (formatSentence s), tail (formatSentence s),[])
-
-checkSameVariable :: String -> Environment -> Bool
-checkSameVariable var (x:xs)
-    | var == fst x = True || checkSameVariable var xs
-    | var /= fst x = False || checkSameVariable var xs
-checkSameVariable _ [] = False
-
-{-
-getMatrix :: Sentence -> Environment -> String
-
-
-evalLoop :: State -> [[Int]] -> String
-evalLoop (current, rest, e) int_list 
-    | isReturn current = getMatrix current e
-    | !(isReturn current) = evalLoop (eval (current, rest, e))
--}
-
-eval :: State -> State
--- SplIntDeclare  Int i = 3
-eval ( (SplIntDeclare var (SplInt x) ),rest, e) 
-    | checkSameVariable var e = error "same name variables"
-    | length rest > 0 = eval (head rest, tail rest, (var,(show x), TyInt):e )
-    | otherwise = error "invalid last line"
-eval ( (SplIntDeclare var _ ),rest, e) = error "Invalid variable, Int expected"
-
--- SplBoolDeclare  Bool x = false
-eval ( (SplBoolDeclare var SplTrue ),rest, e) 
-    | checkSameVariable var e = error "same name variables"
-    | length rest > 0 = eval (head rest, tail rest, (var, "True", TyBool):e )
-    | otherwise = error "invalid last line"
-eval ( (SplBoolDeclare var SplFalse ),rest, e) 
-    | checkSameVariable var e = error "same name variables"
-    | length rest > 0 = eval (head rest, tail rest, (var, "False", TyBool):e )
-    | otherwise = error "invalid last line"
-eval ( (SplBoolDeclare var _),rest, e) = error "Invalid variable, Bool expected"
-
--- SplIntListDeclare1   Int[] x = [1,2,3]
-eval ( (SplIntListAssignment var (SplIntList x)) , rest, e)
-    | checkSameVariable var e = error "same name variables"
-    | length rest > 0 = eval (head rest, tail rest, (var, (show x), TyIntList):e )
-    | otherwise = error "invalid last line"
-
--- SplIntListDeclare2   Int[] x
-eval ((SplIntListDeclare var), rest, e )
-    | checkSameVariable var e = error "same name variables"
-    | length rest > 0 = eval (head rest, tail rest, (var, "[]", TyIntList):e )
-    | otherwise = error "invalid last line"
-
--- SplIntMatrixDeclare Int[][] x
-eval ( (SplIntMatrixDeclare var), rest, e)
-    | checkSameVariable var e = error "same name variables"
-    | length rest > 0 = eval (head rest, tail rest, (var, "[]", TyIntMatrix):e )
-    | otherwise = error "invalid last line"
-
-
-
-
-
-
+    data SplType = TyInt | TyBool | TyIntList | TyIntMatrix 
+        deriving (Show,Eq)
     
-
-
---Less Than Exp reduction
-evalExpr :: Expr -> Expr
-evalExpr (SplLessThan (SplInt n1) (SplInt n2)) = getBool (n1 < n2) 
-evalExpr (SplLessThan e1 e2) =  evalExpr (SplLessThan (evalExpr e1) (evalExpr e2))
-
---Add Exp reduction
-evalExpr (SplAdd (SplInt n1) (SplInt n2)) = SplInt (n1+n2)
-evalExpr (SplAdd e1 e2) = evalExpr (SplAdd (evalExpr e1) (evalExpr e2))
-
---Substract Exp reduction
-evalExpr (SplSubtract (SplInt n1) (SplInt n2)) = SplInt (n1-n2)
-evalExpr (SplSubtract e1 e2) = evalExpr (SplSubtract (evalExpr e1) (evalExpr e2))
-
---Divide Exp reduction
-evalExpr (SplDivide (SplInt n1) (SplInt n2)) = SplInt (n1 `div` n2) 
-evalExpr (SplDivide e1 e2) = evalExpr (SplDivide (evalExpr e1) (evalExpr e2))
-
-evalExpr (SplInt n) = (SplInt n)
-
-getBool :: Bool -> Expr
-getBool e 
-    | e == True = SplTrue
-    | e == False = SplFalse
+    type Environment = [(String,String,SplType)]
+    type State = (Sentence,[Sentence],Environment)
+    
+    formatSentence :: Sentence -> [Sentence]
+    formatSentence (SplConnecting x1 x2) = formatSentence x1 ++ formatSentence x2
+    formatSentence x = [x]
+    
+    parseToState :: Sentence -> State
+    parseToState s = (head (formatSentence s), tail (formatSentence s),[])
+    
+    
+    {-
+    getMatrix :: Sentence -> Environment -> String
+    
+    
+    evalLoop :: State -> [[Int]] -> String
+    evalLoop (current, rest, e) int_list 
+        | isReturn current = getMatrix current e
+        | !(isReturn current) = evalLoop (eval (current, rest, e))
+    -}
+    
+    eval :: State -> State
+    -- SplIntDeclare  Int i = 3
+    eval ( (SplIntDeclare var (SplInt x) ),rest, e) 
+        | checkSameVariable var e = error "same name variables"
+        | length rest > 0 = eval (head rest, tail rest, (var,(show x), TyInt):e )
+        | otherwise = error "invalid last line"
+    eval ( (SplIntDeclare var _ ),rest, e) = error "Invalid variable, Int expected"
+    
+    -- SplBoolDeclare  Bool x = false
+    eval ( (SplBoolDeclare var SplTrue ),rest, e) 
+        | checkSameVariable var e = error "same name variables"
+        | length rest > 0 = eval (head rest, tail rest, (var, "True", TyBool):e )
+        | otherwise = error "invalid last line"
+    eval ( (SplBoolDeclare var SplFalse ),rest, e) 
+        | checkSameVariable var e = error "same name variables"
+        | length rest > 0 = eval (head rest, tail rest, (var, "False", TyBool):e )
+        | otherwise = error "invalid last line"
+    eval ( (SplBoolDeclare var _),rest, e) = error "Invalid variable, Bool expected"
+    
+    -- SplIntListDeclare1   Int[] x = [1,2,3]
+    eval ( (SplIntListAssignment var (SplIntList x)) , rest, e)
+        | checkSameVariable var e = error "same name variables"
+        | length rest > 0 = eval (head rest, tail rest, (var, (show x), TyIntList):e )
+        | otherwise = error "invalid last line"
+    
+    -- SplIntListDeclare2   Int[] x
+    eval ((SplIntListDeclare var), rest, e )
+        | checkSameVariable var e = error "same name variables"
+        | length rest > 0 = eval (head rest, tail rest, (var, "[]", TyIntList):e )
+        | otherwise = error "invalid last line"
+    
+    -- SplIntMatrixDeclare Int[][] x
+    eval ( (SplIntMatrixDeclare var), rest, e)
+        | checkSameVariable var e = error "same name variables"
+        | length rest > 0 = eval (head rest, tail rest, (var, "[]", TyIntMatrix):e )
+        | otherwise = error "invalid last line"
+    
+    -- SplAssignment      Exp = Exp
+    
+    
+    
+    -- push     x.push(3)
+   
+    
+    
+    
+    --Less Than Exp reduction
+    evalExpr :: Expr -> Expr
+    evalExpr (SplLessThan (SplInt n1) (SplInt n2)) = getBool (n1 < n2) 
+    evalExpr (SplLessThan e1 e2)  =  evalExpr (SplLessThan (evalExpr e1) (evalExpr e2))
+    
+    --Add Exp reduction
+    evalExpr (SplAdd (SplInt n1) (SplInt n2)) = SplInt (n1+n2)
+    evalExpr (SplAdd e1 e2) = evalExpr (SplAdd (evalExpr e1) (evalExpr e2))
+    
+    --Substract Exp reduction
+    evalExpr (SplSubtract (SplInt n1) (SplInt n2)) = SplInt (n1-n2)
+    evalExpr (SplSubtract e1 e2) = evalExpr (SplSubtract (evalExpr e1) (evalExpr e2))
+    
+    --Divide Exp reduction
+    evalExpr (SplDivide (SplInt n1) (SplInt n2)) = SplInt (n1 `div` n2) 
+    evalExpr (SplDivide e1 e2) = evalExpr (SplDivide (evalExpr e1) (evalExpr e2))
+    
+    evalExpr (SplInt n) = (SplInt n)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    checkSameVariable :: String -> Environment -> Bool
+    checkSameVariable var (x:xs)
+        | var == get1st x = True || checkSameVariable var xs
+        | var /= get1st x = False || checkSameVariable var xs
+    checkSameVariable _ [] = False
+    
+    getBool :: Bool -> Expr
+    getBool e 
+        | e == True = SplTrue
+        | e == False = SplFalse
+    
+    get1st (a,_,_) = a
+    
+    get2nd (_,b,_) = b
+    
+    get3rd (_,_,c) = c
+        
+    
