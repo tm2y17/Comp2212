d563446139842d570b53bb6a965cd68623426b99
Zecheng <zc312060172@gmail.com>
2019-03-17 02:19:36 +0000

    hh

--- SplEval.hs
+++ SplEval.hs
@@ -82,3 +82,27 @@ isSplIntList _ = False
 isReturn :: Sentence -> Bool
 isReturn (SplReturn Expr) = True
 isReturn _ = False
+
+ --Less Than Exp reduction
+ evalExpr :: Expr -> Expr
+ evalExpr (SplLessThan (SplInt n1) (SplInt n2)) = getBool (n1 < n2) 
+ evalExpr (SplLessThan e1 e2) =  evalExpr (SplLessThan (evalExpr e1) (evalExpr e2))
+
+ --Add Exp reduction
+ evalExpr (SplAdd (SplInt n1) (SplInt n2)) = SplInt (n1+n2)
+ evalExpr (SplAdd e1 e2) = evalExpr (SplAdd (evalExpr e1) (evalExpr e2))
+ 
+ --Substract Exp reduction
+ evalExpr (SplSubtract (SplInt n1) (SplInt n2)) = SplInt (n1-n2)
+ evalExpr (SplSubtract e1 e2) = evalExpr (SplSubtract (evalExpr e1) (evalExpr e2))
+
+ --Divide Exp reduction
+ evalExpr (SplDivide (SplInt n1) (SplInt n2)) = SplInt (n1 `div` n2) 
+ evalExpr (SplDivide e1 e2) = evalExpr (SplDivide (evalExpr e1) (evalExpr e2))
+
+ evalExpr (SplInt n) = (SplInt n)
+
+ getBool :: Bool -> Expr
+ getBool e 
+     | e == True = SplTrue
+     | e == False = SplFalse
